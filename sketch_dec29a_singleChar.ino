db3b5b21cd5c615653fd7b67f5ce440a079c4a87
Daniel <elmokono@hotmail.com>
2018-12-31 20:29:41 -0300

    working!!!!

--- sketch_dec29a_singleChar.ino
+++ sketch_dec29a_singleChar.ino
@@ -1,17 +1,37 @@
-#include <SoftwareSerial.h>  //Software Serial Port
-#define RxD 1
-#define TxD 2
+#include <SoftwareServo.h>
+#include <SoftwareSerial.h>
+
+//at tiny pins ///////////////////////////////
+#define servopin 0
+#define RxD 2
+#define TxD 1
+#define tip120 3
+#define led 4
+//////////////////////////////////////////////
 
 SoftwareSerial Serial(RxD, TxD);
+SoftwareServo servo;
 
-int led = 4;
+unsigned long servoRefresh;
 
 void setup()
 {
   Serial.begin(9600);
-
+  servo.attach(servopin);
   pinMode(led, OUTPUT);
-  digitalWrite(led, HIGH);
+  pinMode(tip120, OUTPUT);
+  pinMode(servopin, OUTPUT);
+
+  digitalWrite(led, HIGH);  
+}
+
+void refreshServo()
+{
+  //servo refresh every 50ms
+  if (millis() - servoRefresh > 50) {
+    servoRefresh = millis();
+    SoftwareServo::refresh();
+  }
 }
 
 void loop()
@@ -22,18 +42,29 @@ void loop()
     rc = Serial.read();
     if (rc == 'W') digitalWrite(led, HIGH); //lights on
     if (rc == 'w') digitalWrite(led, LOW); //lights off
-    if (rc == 'F') //forward
+    if (rc == 'F' || rc == 'G' || rc == 'I') //forward
     {
+      digitalWrite(tip120, HIGH); //gas
+    }
+    if (rc == 'F' ) //forward
+    {
+      servo.write(90);
     }
     if (rc == 'S') //stop
     {
+      digitalWrite(tip120, LOW); //stop
     }
-    if (rc == 'L') //left
+    if (rc == 'L' || rc == 'G') //left
     {
+      servo.write(140);
     }
-    if (rc == 'R') //right
+    if (rc == 'R' || rc == 'I') //right
     {
+      servo.write(40);
     }
   }
+
+  refreshServo();
+
 }
 
